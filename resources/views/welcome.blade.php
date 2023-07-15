d88080f439f5f85f2b4f557f8f04fcdc10cff179
atree-rohit <rohit.george@atree.org>
2023-07-15 13:40:40 +0530

    page slug

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,10 @@
         @vite('resources/css/app.css')
     </head>
     <body>
-        <div id="app"></div>
+        <div id="app" />
         @vite('resources/js/app.js')
+        <script>
+            window.page = @json($page);
+        </script>
     </body>
 </html>
