b6bf2d833ca7dfaf168490aeb6f3ff2c6cf22b16
atree-rohit <rohit.george@atree.org>
2023-07-15 12:59:23 +0530

    vue components

---
+++ resources/js/App.vue
@@ -0,0 +1,55 @@
+<style scoped>
+    .main-container{
+        /* border: 1px solid red; */
+        width: 100%;
+        height: 100vh;
+        display: grid;
+        grid-template-rows: 5fr 80fr 7.5fr;
+    }
+    .main-container .main-content,
+    .main-container footer
+    {
+        display: grid;
+        justify-content: center;
+        align-items: center;
+        font-size:2rem;
+    }
+    nav{
+        border: 1px solid blue;
+    }
+    .main-content{
+        border: 1px solid green;
+    }
+    footer{
+        border: 1px solid yellow;
+    }
+</style>
+
+<template>
+    <div class="main-container">
+        <navigation />
+        <div class="main-content">
+            Main
+        </div>
+        <footer>
+            Footer
+        </footer>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import Navigation from "./components/Navigation.vue"
+
+export default defineComponent({
+    name: "App",
+    components: {Navigation},
+    data(){
+        return {
+        }
+    },
+    mounted(){
+        // console.clear()
+    }
+})
+</script>
---
+++ resources/js/components/Navigation.vue
@@ -0,0 +1,135 @@
+<style scoped>
+nav{
+    display: flex;
+    justify-content: space-between;
+    background-color: var(--primary);
+}
+nav .logo{
+        display: grid;
+        justify-content: center;
+        align-items: center;
+    }
+.logo{
+    width: 7.5%;
+    border: 1px solid red;
+}
+
+ul{
+    max-width: 75rem;
+    list-style: none;
+    display:flex;
+    justify-content: space-around;
+    align-items:baseline;
+}
+ul li{
+    border-radius: .33rem;
+    padding: .25rem 1rem;
+    margin: auto 0.125rem;
+    transition: all var(--transition-time);
+}
+ul li a{
+    text-decoration: none;
+    color: var(--primary-text);
+    white-space: nowrap;
+    transition: all var(--transition-time);
+}
+
+ul li a::before, 
+ul li a::after{
+    content: "";
+    display:block;
+    height: 0rem;
+}
+
+ul li.active a{
+    color: var(--active-clr);
+}
+ul li.active a::before, 
+ul li.active a::after,
+ul li:hover a::before,
+ul li:hover a::after
+{
+    width: 100%;
+    height: .1rem;
+    margin: 0.5rem 0;
+    transition: all var(--transition-time);
+}
+ul li.active a::before, 
+ul li.active a::after{
+    background-color: var(--active-clr);
+}
+
+ul li:hover a{
+    color: yellow;
+}
+ul li:hover a::before,
+ul li:hover a::after{
+    background-color: yellow;
+}
+</style>
+<template>
+    <nav>
+        <div class="logo">Logo</div>
+        <ul>
+            <li
+                v-for="link in links"
+                :key="link.id"
+                :class="{active: link.id == current_link}"
+            >
+                <a :href="link.link">{{ link.label }}</a>
+            </li>
+        </ul>
+    </nav>
+</template>
+   
+<script>
+import {defineComponent} from 'vue'
+export default defineComponent({
+    name: "Navigation",
+    data(){
+        return {
+            links: [
+                {
+                    id: 1,
+                    label: "Home",
+                    link: "/"
+                },
+                {
+                    id: 2,
+                    label: "Data",
+                    link: "/data"
+                },
+                {
+                    id: 3,
+                    label: "About us",
+                    link: "/about"
+                },
+                {
+                    id: 4,
+                    label: "How to Participate",
+                    link: "/participate"
+                },
+                {
+                    id: 5,
+                    label: "Registration",
+                    link: "/registration"
+                },
+                {
+                    id: 6,
+                    label: "Resources",
+                    link: "/resources"
+                },
+                {
+                    id: 7,
+                    label: "Calendar",
+                    link: "/calendar"
+                }
+            ],
+            current_link: 1
+        }
+    },
+    mounted() {
+        console.log('Navigation Component mounted.')
+    }
+})
+</script>
