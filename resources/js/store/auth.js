2a1cb8ccc75c2488b70bee7f9709afc8e012c993
atree-rohit <rohit.george@atree.org>
2023-07-18 14:36:02 +0530

    mutations and redirect on login

--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -14,12 +14,21 @@ export default {
             return state.user
         }
     },
+    mutations: {
+        SET_AUTHENTICATED(state, value){
+            state.authenticated = value
+        },
+        SET_USER(state, value){
+            state.user = value
+        }
+    },
     actions: {
         async login({commit}){
             try {
                 const { data } = await axios.get('/api/user')
                 commit('SET_USER', data)
                 commit('SET_AUTHENTICATED', true)
+                window.location.href = window.location.origin
             } catch ({ response: { data: data_1 } }) {
                 commit('SET_USER', {})
                 commit('SET_AUTHENTICATED', false)
