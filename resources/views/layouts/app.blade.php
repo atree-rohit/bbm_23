074797652016277acd868738e75242dd025d7c67
atree-rohit <rohit.george@atree.org>
2023-07-18 14:36:22 +0530

    added bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,14 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Big Butterfly Month - @yield('title')</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         @vite('resources/css/app.css')
         @vite('resources/js/app.js')
+        @yield('style')
         @stack('scripts')
     </head>
     <body>
-        <div class="container">
+        <div class="app-container">
             @include('layouts.nav')
             <div id="app">
                 <nav></nav>
