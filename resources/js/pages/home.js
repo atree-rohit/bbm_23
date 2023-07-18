634d37c6c08f89bfdfb45c00d9c8c75af605fd25
atree-rohit <rohit.george@atree.org>
2023-07-18 12:20:43 +0530

    added vuex

--- resources/js/pages/home.js
+++ resources/js/pages/home.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
+import store from '../store'
 import Home from '../components/Home.vue'
 
 const app = createApp({});
 app.component('home', Home);
+app.use(store);
 app.mount('#app');
