187192ed4c6874edc5eb8543bd1abc832f08b87b
atree-rohit <rohit.george@atree.org>
2023-07-18 12:20:12 +0530

    renamed component to check if unused

--- resources/js/components/old_Navigation.vue
+++ resources/js/components/old_Navigation.vue
@@ -1,71 +1,71 @@
 <style scoped>
-nav{
-    display: flex;
-    justify-content: space-between;
-    background-color: var(--primary);
-}
-nav .logo{
-        display: grid;
-        justify-content: center;
-        align-items: center;
+    nav{
+        display: flex;
+        justify-content: space-between;
+        background-color: var(--primary);
     }
-.logo{
-    width: 7.5%;
-    border: 1px solid red;
-}
+    nav .logo{
+            display: grid;
+            justify-content: center;
+            align-items: center;
+        }
+    .logo{
+        width: 7.5%;
+        border: 1px solid red;
+    }
 
-ul{
-    max-width: 75rem;
-    list-style: none;
-    display:flex;
-    justify-content: space-around;
-    align-items:baseline;
-}
-ul li{
-    border-radius: .33rem;
-    padding: .25rem 1rem;
-    margin: auto 0.125rem;
-    transition: all var(--transition-time);
-}
-ul li a{
-    text-decoration: none;
-    color: var(--primary-text);
-    white-space: nowrap;
-    transition: all var(--transition-time);
-}
+    ul{
+        max-width: 75rem;
+        list-style: none;
+        display:flex;
+        justify-content: space-around;
+        align-items:baseline;
+    }
+    ul li{
+        border-radius: .33rem;
+        padding: .25rem 1rem;
+        margin: auto 0.125rem;
+        transition: all var(--transition-time);
+    }
+    ul li a{
+        text-decoration: none;
+        color: var(--primary-text);
+        white-space: nowrap;
+        transition: all var(--transition-time);
+    }
 
-ul li a::before, 
-ul li a::after{
-    content: "";
-    display:block;
-    height: 0rem;
-}
+    ul li a::before, 
+    ul li a::after{
+        content: "";
+        display:block;
+        height: 0rem;
+    }
 
-ul li.active a{
-    color: var(--active-clr);
-}
-ul li.active a::before, 
-ul li.active a::after,
-ul li:hover a::before,
-ul li:hover a::after
-{
-    width: 100%;
-    height: .1rem;
-    margin: 0.5rem 0;
-    transition: all var(--transition-time);
-}
-ul li.active a::before, 
-ul li.active a::after{
-    background-color: var(--active-clr);
-}
+    ul li.active a{
+        color: var(--active-clr);
+    }
+    ul li.active a::before, 
+    ul li.active a::after,
+    ul li:hover a::before,
+    ul li:hover a::after
+    {
+        width: 100%;
+        height: .1rem;
+        margin: 0.5rem 0;
+        transition: all var(--transition-time);
+    }
+    ul li.active a::before, 
+    ul li.active a::after{
+        background-color: var(--active-clr);
+    }
 
-ul li:hover a{
-    color: yellow;
-}
-ul li:hover a::before,
-ul li:hover a::after{
-    background-color: yellow;
-}
+    ul li:hover a{
+        color: yellow;
+    }
+    ul li:hover a::before,
+    ul li:hover a::after{
+        background-color: yellow;
+    }
 </style>
 <template>
     <nav>
@@ -99,36 +99,34 @@ export default defineComponent({
                     id: 1,
                     label: "Home",
                     link: "/"
-                },
-                {
+                },{
                     id: 2,
                     label: "Data",
                     link: "/data"
-                },
-                {
+                },{
                     id: 3,
                     label: "About us",
                     link: "/about"
-                },
-                {
+                },{
                     id: 4,
                     label: "How to Participate",
                     link: "/participate"
-                },
-                {
+                },{
                     id: 5,
                     label: "Registration",
                     link: "/registration"
-                },
-                {
+                },{
                     id: 6,
                     label: "Resources",
                     link: "/resources"
-                },
-                {
+                },{
                     id: 7,
                     label: "Calendar",
                     link: "/calendar"
+                },{
+                    id: 8,
+                    label: "Login",
+                    link: "/login"
                 }
             ],
             current_link: 1
@@ -136,6 +134,7 @@ export default defineComponent({
     },
     mounted() {
         this.set_current_link()
+        console.log(555)
     },
     methods:{
         set_current_link(){
