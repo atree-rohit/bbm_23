169b446527cf14be14d0655b9720a5e8d36a4e78
atree-rohit <rohit.george@atree.org>
2023-07-18 12:23:45 +0530

    removed ;

--- resources/js/pages/home.js
+++ resources/js/pages/home.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import store from '../store'
 import Home from '../components/Home.vue'
 
-const app = createApp({});
-app.component('home', Home);
-app.use(store);
-app.mount('#app');
+const app = createApp({})
+app.component('home', Home)
+app.use(store)
+app.mount('#app')
