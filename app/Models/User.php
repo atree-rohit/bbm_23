f73ee3d1af677b64c32303eaa57c66eebab563ee
atree-rohit <rohit.george@atree.org>
2023-07-19 10:09:03 +0530

    user-

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Resource::class, 'added_by');
     }
+
+    public function files()
+    {
+        return $this->hasMany(FileUpload::class, 'added_by');
+    }
 }
