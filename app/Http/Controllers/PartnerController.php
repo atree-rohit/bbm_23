27356b6f4d4a229ea045d178653dd8cb2f13919f
atree-rohit <rohit.george@atree.org>
2023-07-20 09:29:45 +0530

    cleaning

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -19,25 +19,6 @@ class PartnerController extends Controller
         });
         return $all_data;
     }
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $request->validate([
@@ -76,36 +57,5 @@ class PartnerController extends Controller
                 'status' => 404,
             ]);
         }
-    }
-    /**
-     * Display the specified resource.
-     */
-    public function show(Partner $partner)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Partner $partner)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Partner $partner)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Partner $partner)
-    {
-        //
     }
 }
