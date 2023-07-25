1bef687d1e9d875fa164d981777df60173f294a7
atree-rohit <rohit.george@atree.org>
2023-07-25 13:06:35 +0530

    force push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
     </head>
     <body>
         <div id="app" />
-        @vite('resources/js/app.js')+
+        @vite('resources/js/app.js')
         <script>
             window.page = @json($page);
         </script>
