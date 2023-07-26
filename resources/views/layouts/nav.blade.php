652dfb9fb7d0c91546daeaac59c56c637dc57b8f
atree-rohit <rohit.george@atree.org>
2023-07-26 16:45:08 +0530

    bootstrap nav

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -28,10 +28,16 @@
         ]
     ];
     if(auth()->user()){
-        $links[] = [
-            "id" => 7,
-            "label" => "Logout",
-            "link" => "/logout"
+        $auth_links = [
+            [
+                "id" => 0,
+                "label" => "Manage Users",
+                "link" => "/manage-users"
+            ],[
+                "id" => 1,
+                "label" => "Logout",
+                "link" => "/logout"
+            ]
         ];
     }
     $current_path = "/";
@@ -41,24 +47,42 @@
         }
     }
     if($current_path == "/"){
-        if($path == "register" || $path == "logout"){
-            $current_path = "/login";
+        if($path == "register" || $path == "register" || $path == "manage-users" || $path == "logout"){
+            $current_path = "auth";
         }
-    }    
+    }
 @endphp
-<nav>
 
-    
-    <div class="logo">
-        <img src="./img/bbm_logo.png" alt="">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="./img/bbm_logo.png" alt="" height="50">
+        </a>
+        <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                @foreach($links as $link)
+                    <li class="nav-item text-end">
+                        <a class="nav-link @if($current_path == $link['link']) active @endif" aria-current="page" href="{{$link['link']}}">
+                            {{$link["label"]}}
+                        </a>
+                    </li>
+                @endforeach
+                @if(auth()->user())
+                    <li class="nav-item dropdown text-end">
+                        <a class="nav-link dropdown-toggle @if($current_path == 'auth') active @endif" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        {{auth()->user()->name}}
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            @foreach($auth_links as $link)
+                                <li><a class="dropdown-item" href="{{$link['link']}}">{{$link['label']}}</a></li>
+                            @endforeach
+                        </ul>
+                    </li>
+                @endif
+        </ul>
+      </div>
     </div>
-    <ul class="navigation">
-        @foreach($links as $link)
-        <li @if($current_path == $link['link']) class='active' @endif>
-            <a href="{{$link['link']}}">
-                <span>{{$link["label"]}}</span>
-            </a>
-        </li>
-        @endforeach
-    </ul>
-</nav>
+  </nav>
