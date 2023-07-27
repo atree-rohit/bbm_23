570afbd62ddc5ce85aed63caecb74aab8c799471
atree-rohit <rohit.george@atree.org>
2023-07-27 15:00:21 +0530

    content

--- resources/js/components/HowToParticipate.vue
+++ resources/js/components/HowToParticipate.vue
@@ -226,14 +226,19 @@ export default defineComponent({
                         {
                             "name": "Organize an event",
                             "caption": [
-                                "Organizing events creates opportunities for people to connect with nature, fosters a sense of community, and raises awareness about the conservation of butterflies and their habitats.",
+                                "Big Butterfly Month is the perfect time for butterfly enthusiasts to come together and rejoice in the magic of butterflies. Through invigorating butterfly walks, engaging talks by experts, captivating contests for children, and more, our shared love for these gentle creatures unites us in a mission to protect and cherish their beauty.",
+                                "Promoting Big Butterfly Month brings numerous advantages, fostering awareness about butterfly conservation on a grand scale. By hosting events, we empower individuals to become citizen scientists, contributing valuable data to understand butterfly populations better. Let's inspire communities to embrace sustainable practices, creating butterfly-friendly environments that support these winged wonders throughout the year.",
+                                "Join the spirited journey of Big Butterfly Month as we foster friendly competition between different states and even districts within states. By encouraging butterfly enthusiasts to record sightings and host events, we can celebrate the diversity of butterflies in each region, boosting local pride while enriching our collective understanding of India's vibrant butterfly ecosystem.",
+                                "In advance, we extend our heartfelt gratitude for your support in spreading the word about Big Butterfly Month. Together, let's make this celebration a resounding success, touching hearts and inspiring actions that will protect and conserve butterflies for generations to come."
                             ],
                             "suggestions": [
-                                "Host awareness programs to educate the public about the importance of butterflies in the ecosystem.",
-                                "Arrange butterfly walks or field trips to explore local butterfly habitats and observe different species.",
-                                "Conduct online talks or webinars featuring butterfly experts to engage a wider audience.",
-                                "Organize contests for children, such as butterfly drawing competitions or butterfly-themed quizzes.",
-                                "Use social media and online platforms to reach out to more people and spread awareness about butterflies."
+                                "Host butterfly walks, camps, and counts to document diverse species.",
+                                "Collaborate with schools and colleges for butterfly awareness programs.",
+                                "Organize art, photography, essay, and quiz contests celebrating butterflies.",
+                                "Promote Big Butterfly Month on social media and in the press.",
+                                "Partner with local nature clubs and wildlife organizations for events.",
+                                "Offer workshops to improve butterfly identification and gardening skills.",
+                                "Encourage citizen scientists to create personalized butterfly checklists."
                             ],
                             links: [
                                 {
@@ -260,27 +265,47 @@ export default defineComponent({
                     "section": "Identify",
                     "subsections": [
                         {
-                            "name": "Help identify butterflies on citizen science portals",
-                            "suggestions": [
-                                "Register on citizen science platforms and contribute to the identification of butterfly observations.",
-                                "Utilize field guides and online resources to enhance your butterfly identification skills.",
-                                "Collaborate with experts and fellow enthusiasts on the portals to verify identifications."
+                            "name": "Identify butterflies on citizen science portals",
+                            "caption": [
+                                "The beauty of citizen science portals lies in their ability to unite experts and enthusiasts in a common pursuit: identifying butterflies. Experts, armed with their specialized knowledge, play a crucial role in validating and verifying the observations submitted by contributors. This collaborative process ensures the accuracy and reliability of the data, which becomes increasingly valuable for research and conservation efforts. As more identification interactions occur, contributors have the unique opportunity to learn about the species they encounter. This exercise not only enhances their understanding of butterflies but also encourages them to deepen their involvement in citizen science initiatives. The interplay between experts, enthusiasts, and contributors enriches the data and propels the advancement of butterfly research nationwide.",
+                                "The interactive identification process on citizen science portals empowers butterfly observation contributors in multiple ways. As they witness their sightings being identified and validated by experts, they gain confidence in their abilities and feel a stronger connection to the broader community of butterfly enthusiasts. The platform serves as a valuable learning tool, enabling contributors to enhance their butterfly identification skills through feedback and insights shared by experts. This collaborative and educational experience encourages observation contributors to engage more actively with butterfly conservation initiatives and fosters a thriving community of passionate butterfly enthusiasts and experts united in safeguarding their future."
                             ],
-                            "caption": "Assisting in identifying butterflies on citizen science portals contributes to the accuracy of data, aiding in more reliable research and conservation efforts."
+                            "suggestions": [
+                                "Explore citizen science portals and actively participate in butterfly identification activities.",
+                                "Witness your butterfly sightings being identified and validated by experts, building confidence and a sense of community.",
+                                "Even if you're unable to identify the species, help tag experts who can assist in the identification process. Your contribution aids in connecting observations with knowledgeable identifiers.",
+                                "If you can only identify butterflies to a certain level, such as family or genus, don't hesitate to contribute. Your partial identification still adds valuable data to the research. Additionally, consider providing constructive feedback to contributors with suggestions on how to improve their photographs and observations, promoting better data quality and enhancing the overall citizen science experience.",
+                                "Take advantage of the learning opportunities provided by the platform to enhance your butterfly identification skills.",
+                                "Be part of a thriving community of butterfly enthusiasts and experts, united in their passion for butterflies and dedicated to their conservation."
+                            ],
+                            links: [
+                                {
+                                    "name": "iNaturalist",
+                                    "link": "https://www.inaturalist.org/",
+                                    "next_link": "https://www.inaturalist.org/projects/big-butterfly-month-india-2023"
+                                },{
+                                    "name": "India Biodiversity Portal",
+                                    "link": "https://www.indiabiodiversity.org/",
+                                }
+                            ]
                         },
                         {
-                            "name": "Identify potential participants",
-                            "suggestions": [
-                                "Reach out to schools, colleges, and nature clubs to identify potential participants who can join BBM.",
-                                "Identify individuals with a keen interest in nature, wildlife, or conservation and encourage them to take part.",
-                                "Engage with social media influencers or environmental activists to spread the word about BBM and attract more participants."
+                            "name": "Identify potential participants and partners",
+                            "caption": [
+                                "In the spirit of fostering a strong community, we seek to identify key participants and partners who will play pivotal roles in making Big Butterfly Month a resounding success. From passionate butterfly enthusiasts and nature lovers to students and families, each contributor's engagement is crucial in shaping our collective understanding of India's butterfly population. Alongside participants, potential partners such as educational institutions, nature clubs, wildlife organizations, and local communities hold immense potential for collaborating on awareness programs, workshops, and events dedicated to butterfly conservation and citizen science. By identifying these essential participants and allies, we pave the way for a united force, working harmoniously to protect and cherish these mesmerizing winged wonders.",
+                                "Our mission to drive active participation in Big Butterfly Month calls for a concerted effort to connect with individuals and entities who share our vision for a brighter future for butterflies. Through concerted outreach, we aim to engage diverse audiences, inspiring them to embark on butterfly walks, submit observations, and contribute invaluable data to further our conservation goals. Utilizing the power of social media, online forums, and community events, we will spread awareness far and wide, igniting enthusiasm and encouraging active involvement in this important citizen science initiative. Additionally, we identify potential supporters, including environmental influencers, conservationists, and wildlife experts, whose backing will amplify the impact of Big Butterfly Month, inspiring others to join us in safeguarding these delicate wonders and preserving their habitats for generations to come."
                             ],
-                            "caption": "Identifying potential participants and advocates helps in expanding the BBM community, amplifying its impact, and fostering a wider interest in butterfly conservation."
+                            "suggestions": [
+                                "Reach out to butterfly enthusiasts, nature lovers, students, and families as potential participants for Big Butterfly Month. Encourage them to engage in butterfly walks, submit observations, and contribute to butterfly conservation.",
+                                "Identify educational institutions, nature clubs, wildlife organizations, and local communities as potential partners. Collaborate with them to organize awareness programs, workshops, and events that promote butterfly conservation and citizen science.",
+                                "Use social media, online forums, and community events to reach out to potential participants and partners. Leverage digital platforms and local networks to spread awareness and foster active participation in Big Butterfly Month.",
+                                "Explore partnerships with environmental influencers, conservationists, and wildlife experts to champion the cause of butterfly conservation. Their support can amplify the impact of Big Butterfly Month and inspire others to join the movement for protecting these delicate wonders."
+                            ]
                         }
                     ]
                 }
             ],
-            show_section: 1,
+            show_section: 2,
         }
     },
     computed: {
