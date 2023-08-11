d22fdab76ea048ba8b0eb96942f24fc21f847d93
atree-rohit <rohit.george@atree.org>
2023-08-11 16:53:51 +0530

    submit form

--- app/Http/Controllers/CountFormController.php
+++ app/Http/Controllers/CountFormController.php
@@ -3,10 +3,59 @@
 namespace App\Http\Controllers;
 
 use App\Models\CountForm;
+use App\Models\CountSpecies;
 use Illuminate\Http\Request;
 
 class CountFormController extends Controller
 {
+    public function submit_form(Request $request)
+    {
+        $coordinates = explode(',', $request->coordinates);
+        $form = new CountForm();
+        $form->name = $request->name;
+        $form->affilation = $request->affilation;
+        $form->phone = $request->phone;
+        $form->email = $request->email;
+        $form->team_members = $request->team_members;
+        $form->photo_link = $request->photo_link;
+        $form->location = $request->location;
+        $form->state = $request->state;
+        $form->district = $request->district;
+        $form->coordinates = $request->coordinates;
+        $form->latitude = $coordinates[0];
+        $form->longitude = $coordinates[0];
+        $form->date = $request->date;
+        $form->start_time = $request->start_time;
+        $form->end_time = $request->end_time;
+        $form->altitude = $request->altitude;
+        $form->distance = $request->distance;
+        $form->weather = $request->weather;
+        $form->comments = $request->comments;
+        $form->status = 'pending';
+        $form->validated = false;
+        $form->save();
+
+        foreach($request->species_list as $k=>$sl){
+            $species = new CountSpecies();
+            $species->count_form_id = $form->id;
+            $species->no = $k + 1;
+            $species->common_name = $sl["common_name"];
+            $species->scientific_name = $sl["scientific_name"];
+            $species->individuals = $sl["individuals"];
+            $species->remarks = $sl["remarks"];
+            $species->status = 'pending';
+            $species->validated = false;
+            $species->save();
+        }
+
+        return response()->json([
+            'message' => 'Form submitted successfully',
+            'form' => $form
+        ]);
+        
+
+        
+    }
     /**
      * Display a listing of the resource.
      */
