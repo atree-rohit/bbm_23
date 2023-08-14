17f051b291d2628254c2ce84b672d55275627347
atree-rohit <rohit.george@atree.org>
2023-08-14 16:34:37 +0530

    nrb

--- public/build/assets/logout-a864e250.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/build/assets/logout-a864e250.js
-import{g as t,e as n,_ as e,c as s,o as a,f as c,s as p}from"./_plugin-vue_export-helper-2b3bbbef.js";const i=t({name:"Logout",methods:{...n({signOut:"auth/logout"})},mounted(){this.signOut(),window.location.href=window.location.origin}});function r(m,g,l,_,d,f){return a(),s("h1",null,"Logout")}const u=e(i,[["render",r]]),o=c({});o.component("logout",u);o.use(p);o.mount("#app");
-========
-import{g as t,e as n,_ as e,c as s,o as a,f as c,s as p}from"./_plugin-vue_export-helper-b0ae1e6c.js";const i=t({name:"Logout",methods:{...n({signOut:"auth/logout"})},mounted(){this.signOut(),window.location.href=window.location.origin}});function r(m,g,l,_,d,f){return a(),s("h1",null,"Logout")}const u=e(i,[["render",r]]),o=c({});o.component("logout",u);o.use(p);o.mount("#app");
->>>>>>>> count-app:public/build/assets/logout-6c6d71cb.js
