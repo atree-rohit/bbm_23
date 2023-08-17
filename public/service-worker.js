c2e83988727528f2f7851bcdbcdc76927fe47152
atree-rohit <rohit.george@atree.org>
2023-08-17 06:33:52 +0530

    sw tweak

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,18 +1,18 @@
 // service-worker.js
 const CACHE_NAME = 'butterfly-checklist-cache';
 const urlsToCache = [
-    new Request('/'),
-    new Request('/app.css'),
-    new Request('/app.js'),
+    '/',
+    '/app.css',
+    '/app.js',
     './build/manifest.json',
-  // Include other important assets
+    // Include other important assets
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
-        console.log(1, urlsToCache)
+        console.log(1, urlsToCache);
         return cache.addAll(urlsToCache);
       })
   );
@@ -22,7 +22,20 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        return response || fetch(event.request);
+        if (response) {
+          return response; // If cached, return the cached response
+        }
+
+        // If not cached, fetch from the network
+        return fetch(event.request)
+          .then((networkResponse) => {
+            // Cache the fetched response for future use
+            return caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, networkResponse.clone());
+                return networkResponse;
+              });
+          });
       })
   );
 });
