fe2bf505aef22378cc982daaa675e4c90794bbfb
atree-rohit <rohit.george@atree.org>
2023-08-17 06:30:30 +0530

    load service worker

--- resources/js/pages/butterfly_counts.js
+++ resources/js/pages/butterfly_counts.js
@@ -7,15 +7,15 @@ app.component('butterfly-counts', ButterflyCounts)
 app.use(store)
 app.mount('#app')
 
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', () => {
-//       navigator.serviceWorker.register('/service-worker.js')
-//         .then(registration => {
-//           console.log('Service Worker registered with scope:', registration.scope);
-//         })
-//         .catch(error => {
-//           console.error('Service Worker registration failed:', error);
-//         });
-//     });
-//   }
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(registration => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch(error => {
+          console.error('Service Worker registration failed:', error);
+        });
+    });
+  }
   
