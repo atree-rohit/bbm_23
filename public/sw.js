b428771ff32244a5151d4b23da71a74a7bba55b4
atree-rohit <rohit.george@atree.org>
2023-08-23 17:21:38 +0530

    rename cache

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const CACHE_NAME = 'butterfly-checklist-cache-1.1';
+const CACHE_NAME = 'bbm-counts-app-cache-1';
 const urlsToCache = [
     '/',
     '/build/assets/app-7a30ea22.css',
