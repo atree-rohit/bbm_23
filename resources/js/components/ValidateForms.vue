23a1ac896646d7931cb8901c207cbf766e8153fc
atree-rohit <rohit.george@atree.org>
2023-08-23 16:40:58 +0530

    redid

--- resources/js/components/ValidateForms.vue
+++ resources/js/components/ValidateForms.vue
@@ -1,35 +1,91 @@
+<style scoped>
+.table tbody tr:hover td{
+    cursor: pointer;
+    background-color: hsl(100, 80%, 80%) !important;
+}
+</style>
+
 <template>
-    <div class="validate-forms-container">
-        <count-form-row
-            v-for="form in pending"
-            :key="form.id"
-            :count_form="form"
-        />
+    <div class="container-fluid validate-forms-container">
+        <table class="table table-sm">
+            <thead class="bg-success text-warning">
+                <tr>
+                    <th
+                        v-for="h in headers"
+                        :key="h"
+                        v-text="h"
+                    />
+                </tr>
+            </thead>
+            <tbody class="table-secondary">
+                <tr
+                    v-for="row in filtered_data"
+                    :key="row.id"
+                >
+                    <td
+                        v-for="h in headers"
+                        :key="h"
+                        v-text="row[h]"
+                        @click="selectForm(row.id)"
+                    />
+
+                </tr>
+            </tbody>
+        </table>
     </div>
+    <modal-show-count-form 
+        :show="showModal"
+        :form_data="selected_form"
+        @close="closeModal"
+    />
 </template>
 
 <script>
 import { mapState } from 'vuex'
 import store from '../store'
-import CountFormRow from "./CountFormRow.vue"
+import ModalShowCountForm from "./ModalShowCountForm.vue"
 export default{
     name: "ValidateForms",
     components: {
-        CountFormRow
+        ModalShowCountForm
+    },
+    data(){
+        return{
+            headers: ["location","state", "district", "latitude", "longitude", "date", "start_time", "end_time", "status"],
+            showModal: false,
+            selectedFormID: -1
+        }
     },
     computed: {
         ...mapState({
             user: state => state.auth.user,
             all_data: state => state.count_forms.all_data
         }),
-        pending(){
-            let pending = this.all_data.filter((d) => d.status != 'approved')
-            return pending
-        }
+        filtered_data(){
+            return this.all_data
+
+        },
+        selected_form(){
+            if(this.selectedFormID == -1){
+                return {}
+            }
+            return this.all_data.find((d) => d.id == this.selectedFormID)
+        }        
+    },
+    watch:{
     },
     mounted(){
         store.dispatch('count_forms/getAllData')
     },
-
+    methods:{
+        selectForm(form_id){
+            this.selectedFormID = form_id
+            this.showModal = true
+        },
+        closeModal(){
+            this.selectedForm = {}
+            this.showModal = false
+        }
+    }
 }
 </script>
