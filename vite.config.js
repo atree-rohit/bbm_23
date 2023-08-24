e04ddb75cbba7d0a418a0a4642c5f615e3069681
atree-rohit <rohit.george@atree.org>
2023-08-24 10:23:18 +0530

    nrb

--- vite.config.js
+++ vite.config.js
@@ -35,7 +35,6 @@ export default defineConfig({
             outDir: 'public/build',
             injectRegister: 'auto',
             registerType: 'auto',
-            registerType: 'autoUpdate',
             devOptions: {
                 enabled: true
             },
