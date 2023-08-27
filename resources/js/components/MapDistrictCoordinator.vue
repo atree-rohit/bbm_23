d949807f4fcc181ec8bbfad94e8c3ca2c0b72554
atree-rohit <rohit.george@atree.org>
2023-08-27 20:26:37 +0530

    removed geojsongs

--- resources/js/components/MapDistrictCoordinator.vue
+++ resources/js/components/MapDistrictCoordinator.vue
@@ -19,10 +19,8 @@
 
 <script>
 import { defineComponent } from 'vue'
-import { mapActions, mapState } from 'vuex'
+import { mapState } from 'vuex'
 import MapComponent from './MapComponent.vue'
-import districts from '../json/districts.json'
-import states from '../json/states.json'
 
 export default defineComponent({
     name: "MapDistrictCoordinator",
@@ -32,44 +30,44 @@ export default defineComponent({
     emits: ["polygon-clicked"],
     data() {
         return {
-            geojson: {
-                district: districts,
-                state: states
-            },
-            mode: 'state'
+            mode: 'states'
         }
     },
     computed:{
         ...mapState({
-            district_coordinators: state => state.district_coordinators.all_data
+            district_coordinators: state => state.district_coordinators.all_data,
+            geojson: state => state.data.geojson,
         }),
         map_data(){
-            let all = {
-                states: this.geojson.state.features.map(f => f.properties.state),
-                districts: this.geojson.district.features.map(f => f.properties.district),
-            }
-            let op = {
-                state: all.states.map((s) => {
-                    return {
-                        name: s,
-                        value: 0
-                    }
-                }),
-                district: all.districts.map((d) => {
-                    return {
-                        name: d,
-                        value: 0
-                    }
+            let op = {}
+            if(this.geojson.states.features){
+                let all = {
+                    states: this.geojson.states.features.map(f => f.properties.state),
+                    districts: this.geojson.districts.features.map(f => f.properties.district),
+                }
+                op = {
+                    state: all.states.map((s) => {
+                        return {
+                            name: s,
+                            value: 0
+                        }
+                    }),
+                    district: all.districts.map((d) => {
+                        return {
+                            name: d,
+                            value: 0
+                        }
+                    })
+                }
+                this.district_coordinators.forEach((d) => {
+                    op.state.find((s, i) => d.state == this.valueFromLabel(s.name)).value += 1
+                    op.district.forEach((s) => {
+                        if(d.district == this.valueFromLabel(s.name)){
+                            s.value += 1
+                        }
+                    })
                 })
             }
-            this.district_coordinators.forEach((d) => {
-                op.state.find((s, i) => d.state == this.valueFromLabel(s.name)).value += 1
-                op.district.forEach((s) => {
-                    if(d.district == this.valueFromLabel(s.name)){
-                        s.value += 1
-                    }
-                })
-            })
             
             return op
         }
