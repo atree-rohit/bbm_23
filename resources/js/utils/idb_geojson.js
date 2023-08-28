ca976932c5fe18ef69c495507e99b64b6774e355
atree-rohit <rohit.george@atree.org>
2023-08-28 18:56:46 +0530

    version up

--- resources/js/utils/idb_geojson.js
+++ resources/js/utils/idb_geojson.js
@@ -2,7 +2,7 @@
 
 function openDB(storeName) {
     const DB_NAME = 'BBMCountsDBGeojson';
-    let DB_VERSION = 1; // Ensure that this version is updated when making changes
+    let DB_VERSION = 2; // Ensure that this version is updated when making changes
 
     return new Promise((resolve, reject) => {
         const request = indexedDB.open(DB_NAME, DB_VERSION);
