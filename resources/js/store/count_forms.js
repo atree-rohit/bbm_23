a129d5e0b1e26b75519bdbe0e8cae7498b74e71a
atree-rohit <rohit.george@atree.org>
2023-09-04 10:04:13 +0530

    update data locally, dont get all after each change

--- resources/js/store/count_forms.js
+++ resources/js/store/count_forms.js
@@ -19,6 +19,15 @@ export default {
             }
             state.loading = value
         },
+        SET_FORM_STATUS(state, value){
+            let form = state.all_data.find((d) => d.id == value.id)
+            form.status = value.status
+        },
+        SET_SPECIES_STATUS(state, value){
+            let form = state.all_data.find((d) => d.id == value.form_id)
+            let species = form.species_list.find((s) => s.id == value.id)
+            species.status = value.status
+        }
     },
     actions: {
         async getAllData({commit}){
@@ -36,7 +45,7 @@ export default {
                 .forEach(async (species) => {
                     await dispatch('setSpeciesStatus', { species_id: species.id, status: "approved" })
                 })
-            //Validate form
+            
             await dispatch('setFormStatus', { form_id: form_data.id, status: "approved" })
             commit('SET_LOADING', null)
         },
@@ -45,15 +54,28 @@ export default {
             commit('REMOVE_DATA', form_data)
         },
         async setFormStatus({commit, dispatch}, form_data){
-            commit('SET_LOADING', 'Setting Form Status to ' + form_data.status)
+            commit('SET_LOADING', `Setting Form (#${form_data.form_id}) Status to ` + form_data.status)
             const { data } = await axios.post('/api/count_forms/set_form_status', form_data)
-            await dispatch('getAllData')
+            if(data){
+                let update_data = {
+                    id: data.form.id,
+                    status: data.form.status,
+                }
+                commit('SET_FORM_STATUS', update_data)
+            }
             commit('SET_LOADING', null)
         },
         async setSpeciesStatus({commit, dispatch}, species_data){
-            commit('SET_LOADING', 'Setting Form Status to ' + species_data.status)
+            commit('SET_LOADING', `Setting Species (#${species_data.species_id}) Status to ` + species_data.status)
             const { data } = await axios.post('/api/count_forms/set_species_status', species_data)
-            await dispatch('getAllData')
+            if(data){
+                let update_data = {
+                    id: data.species.id,
+                    form_id: data.species.count_form_id,
+                    status: data.species.status,
+                }
+                commit('SET_SPECIES_STATUS', update_data)
+            }
             commit('SET_LOADING', null)
         }
     }
