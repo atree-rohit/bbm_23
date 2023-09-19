4658eea4c9a095c5bcc61edaaad74a9dc8b6b6c5
atree-rohit <rohit.george@atree.org>
2023-09-19 12:31:39 +0530

    data_with_classes from getter; btn-row for filtering data; sort_data on th click

--- resources/js/components/CleanData.vue
+++ resources/js/components/CleanData.vue
@@ -17,17 +17,29 @@
 </style>
 
 <template>
-    <div class="btn-group border border-secondary w-100 p-3">
-        <button
-            class="btn mx-2 rounded"
-            v-for="portal in portals"
-            :key="portal"
-            :class="{'btn-success': portal === selected_portal, 'btn-outline-secondary': portal !== selected_portal}"
-            v-text="portal"
-            @click="selected_portal = portal"
-        />
+    <div class="btn-rows">
+        <div class="btn-group border border-secondary w-100 p-3">
+            <button
+                class="btn mx-2 rounded"
+                v-for="portal in portals"
+                :key="portal"
+                :class="{'btn-success': portal === selected_portal, 'btn-outline-secondary': portal !== selected_portal}"
+                v-text="portal"
+                @click="selected_portal = (selected_portal == portal) ? null : portal"
+            />
+        </div>
+        <div class="btn-group border border-secondary w-100 p-3">
+            <button
+                class="btn mx-2 rounded"
+                v-for="filter in data_filters"
+                :key="filter"
+                :class="{'btn-success': filter === selected_filter, 'btn-outline-secondary': filter !== selected_filter}"
+                v-text="filter"
+                @click="selected_filter = (selected_filter == filter) ? null : filter"
+            />
+        </div>
+
     </div>
-    {{ data.length }}
     <div class="table-container border border-danger" v-if="selected_portal">
         <table class="table table-sm table-hover">
             <thead class="bg-dark text-light">
@@ -36,12 +48,13 @@
                         v-for="header in headers"
                         :key="header"
                         v-text="header"
+                        @click="sortCol(header)"
                     />
                 </tr>
             </thead>
             <tbody>
                 <tr
-                    v-for="row in unvalidated"
+                    v-for="row in sorted_data"
                     :key="row.id"
                     @click="rowClick(row)"
                     :class="rowClass(row)"
@@ -50,13 +63,14 @@
                         v-for="header in headers"
                         :key="header"
                         v-text="row[header]"
+                        :class="cellClass(row,header)"
                     />
                 </tr>
             </tbody>
         </table>
     </div>
     <MapCleanData
-        :data="unvalidated"
+        :data="filtered_data"
         :modes="['countries', 'states', 'districts']"
         tooltip_third_row_label=""
     />
@@ -66,6 +80,9 @@
         :geojson="geojson"
         @close="closeModal"
     />
+    <pre>
+        {{ data[0] }}
+    </pre>
 </template>
 
 <script setup>
@@ -77,6 +94,10 @@ import MapCleanData from './MapCleanData.vue'
 const store = useStore()
 const portals = ["counts", "inat", "ibp", "ifb"]
 const selected_portal = ref("")
+const data_filters = ["all", "validated", "unvalidated"]
+const selected_filter = ref("all")
+const sort_col = ref("id")
+const sort_dir = ref("asc")
 
 const show_modal = ref(false)
 const modal_observation_data = ref({})
@@ -93,17 +114,69 @@ watch(selected_portal, (newVal, oldVal) => {
 })
 
 const geojson = computed(() => store.state.data.geojson)
-const data = computed(() => store.state.clean_data.data)
+const data = computed(() => store.getters["clean_data/data_with_classes"])
+
+const filtered_data = computed(() => {
+    if(data && data.value && data.value[0]){
+        switch(selected_filter.value){
+            case "all":
+                return data.value
+            case "validated":
+                return data.value.filter(row => row.validated)
+            case "unvalidated":
+                return data.value.filter(row => !row.validated)
+        }
+    }
+    return []
+})
+
+const sorted_data = computed(() => {
+    if (data && data.value && data.value[0]) {
+        return filtered_data.value.sort((a, b) => {
+            const valA = a[sort_col.value];
+            const valB = b[sort_col.value];
+
+            // Check data types
+            const isNumber = (value) => !isNaN(value);
+            const isBoolean = (value) => typeof value === 'boolean';
+
+            if (isNumber(valA) && isNumber(valB)) {
+                // Sort as numbers
+                return sort_dir.value === 'asc' ? valA - valB : valB - valA;
+            } else if (isBoolean(valA) && isBoolean(valB)) {
+                // Sort as booleans (true comes before false)
+                return sort_dir.value === 'asc' ? (valA - valB) : (valB - valA);
+            } else {
+                // Sort as strings (default)
+                return sort_dir.value === 'asc' ? valA.localeCompare(valB) : valB.localeCompare(valA);
+            }
+        });
+    }
+    return [];
+});
+
+
+
+
+
+
 const headers = computed(() => {
     if(data && data.value && data.value[0]){
-        return ["id", "user", "observed_on", "place", "country", "state", "district", "latitude", "longitude", "taxa_id", "validated"]
-        // return Object.keys(data.value[0])
+        return ["id", "user", "observed_on", "place", "country", "state", "district", "latitude", "longitude", "taxa_id", "species", "validated"]
     } else {
         return []
     }
 })
-const unvalidated = computed(() => data.value.filter((d) => d.validated == false))
 
+const sortCol = (col) => {
+    if(sort_col.value === col){
+        sort_dir.value = (sort_dir.value === "asc") ? "desc" : "asc"
+    } else {
+        sort_col.value = col
+        sort_dir.value = "asc"
+    }
+    console.log(col, sort_col.value, sort_dir.value)
+}
 
 const rowClick = (row) => {
     modal_observation_data.value = row
@@ -115,5 +188,33 @@ const closeModal = () => {
     show_modal.value = false    
 }
 
-const rowClass = (row) =>  row.validated ? "table-success" : "table-danger"
+const rowClass = (row) =>  {
+    return row.classes.all ? "table-success" : "table-danger"
+}
+
+const cellClass = (cell, col) => {
+    const classMap = {
+        'observed_on': 'classes.date',
+        'user': 'classes.user',
+        'country': 'classes.location.country',
+        'state': 'classes.location.state',
+        'district': 'classes.location.district',
+        'latitude': 'classes.location.latitude',
+        'longitude': 'classes.location.longitude',
+        'taxa_id': 'classes.taxon.taxa_id',
+        'species': 'classes.taxon.species',
+        'validated': 'validated',
+    };
+
+    const classKey = classMap[col];
+
+    if (classKey) {
+        const hasClass = classKey.split('.').reduce((value, key) => (value && value[key]), cell);
+        return hasClass ? 'table-success' : 'table-danger';
+    }
+
+    return 'table-warning';
+};
+
+
 </script>
