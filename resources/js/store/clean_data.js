440f109a431748e62fa735559dfc34265bf265da
atree-rohit <rohit.george@atree.org>
2023-09-19 12:28:31 +0530

    getter for data_with_classes

--- resources/js/store/clean_data.js
+++ resources/js/store/clean_data.js
@@ -5,8 +5,31 @@ export default {
     state: {
         data: []
     },
+    getters: {
+        data_with_classes(state){
+            let op = []
+            if(state.data && state.data.length){
+                op = state.data.map((d) => {
+                    let classes = {
+                        date: validateDate(d.observed_on),
+                        user: validateUser(d.user),
+                        location: validateLocation(d),
+                        taxon: validateTaxon(d),
+                        all: false
+                    }
+                    classes.all = validateAll(classes)
+                    return {
+                        ...d,
+                        classes
+                    }
+                })
+            }
+            return op
+        }
+    },
     mutations: {
         SET_DATA(state, data){
+            console.log(data)
             state.data = data
         }
     },
@@ -18,4 +41,47 @@ export default {
             }
         }
     }
+}
+
+const validateDate = (date) => {
+    return (!date || /^(202[0-3])-09-\d{2}$/.test(date)) 
+}
+const validateUser = (user) => {
+    return (user && user.length > 0)
+}
+const validateLocation = (d) => {
+    const isIndia = d.country === 'India';
+    const op = {
+        country: !!d.country,
+        state: isIndia ? !!d.state : true,
+        district: isIndia ? !!d.district : true,
+        latitude: !isNaN(d.latitude) && !!d.latitude,
+        longitude: !isNaN(d.longitude) && !!d.longitude,
+        all: false
+    }
+    // op.all = ['country', 'state', 'district', 'latitude', 'longitude'].every(key => !!op[key])
+    op.all = validateSetAll(op)
+    return op
+}
+const validateTaxon = (d) => {
+    let op = {
+        taxa_id: !!d.taxa_id,
+        species: !!d.species,
+        all: false
+    }
+    // op.all = ['taxa_id', 'species'].every((key) => op[key] == true)
+    op.all = validateSetAll(op)
+    return op
+}
+
+const validateSetAll = (classes) => {
+    for (const key in classes) {
+        if(key !== 'all' && classes[key] !== true){
+            return false
+        }
+    }
+    return true
+}
+const validateAll = (classes) => {
+    return (classes.date && classes.user && classes.location.all && classes.taxon.all)
 }
