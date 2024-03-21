1e947d8c8a699213d058970778f0f7a2a0500fb6
atree-rohit <rohit.george@atree.org>
2024-03-21 16:24:11 +0530

    nrb

--- public/build/assets/logout-09cacb84.js
+++
@@ -1,1 +0,0 @@
-import{g as t,e as n,_ as e,c as s,o as a,f as c,s as p}from"./_plugin-vue_export-helper-96c316a0.js";const i=t({name:"Logout",methods:{...n({signOut:"auth/logout"})},mounted(){this.signOut(),window.location.href=window.location.origin}});function r(m,g,l,_,d,f){return a(),s("h1",null,"Logout")}const u=e(i,[["render",r]]),o=c({});o.component("logout",u);o.use(p);o.mount("#app");
